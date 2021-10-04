8d9245715f0cb67051a029b3ca3f873ac46b1f1f
Harman Sran <iharmandeep@gmail.com>
2021-10-03 21:04:53 -0400

    layout

---
+++ todoey/lib/screens/add_task_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  const AddTaskScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFF757575),
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Add'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.lightBlueAccent,
+              ),
+            ),
+          ],
+        ),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
